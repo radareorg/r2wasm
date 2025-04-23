b0cbbf5b75c79e7cfb48bd505c03bcd3c7c8d732
pancake <pancake@nopcode.org>
2025-04-23 18:36:18 +0200

    Typescriptify and esbuild all the things

---
+++ src/bundler.ts
@@ -0,0 +1,25 @@
+import * as esbuild from "npm:esbuild";
+import { denoPlugins } from "jsr:@luca/esbuild-deno-loader";
+
+await esbuild.build({
+  // entryPoints: ["r2wasm.ts", "ansi2html.ts"],
+  entryPoints: ["r2wasm.ts"],
+  bundle: true,
+  format: "iife",
+  //  format: "esm",
+  target: "es2017",
+  platform: "browser",
+  outdir: "dist", // "out.js",
+  globalName: "r2wasm",
+  minify: true,
+  // treeShaking: false,
+  //  keepNames: true,
+  //  plugins: [...denoPlugins()],
+  tsconfigRaw: {
+    compilerOptions: {
+      lib: ["dom"],
+    },
+  },
+});
+
+esbuild.stop();
